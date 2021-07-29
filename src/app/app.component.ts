8e4af5c384b34d7afa8cbca671b1ca4067521142
JansK <kmjanssen1@gmail.com>
2021-07-28 23:37:00 -0400

    more initial setup & styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import {
+  Component
+} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flight-schedule-pro';
+  title = 'Flight Schedule Pro Customer List';
 }
